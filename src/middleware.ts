a0165225ad500417a5690ee9e98a096eda988f88
alfredo.narvaez <alfredo.narvaez@adevinta.com>
2022-11-23 09:47:23 +0100

    try comment middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,12 @@
-// middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  if (!request.cookies.has("next-auth.session-token")) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  // if (!request.cookies.has("next-auth.session-token")) {
+  //   return NextResponse.redirect(new URL("/", request.url));
+  // }
 }
 
-export const config = {
-  matcher: "/app/:path*",
-};
+// export const config = {
+//   matcher: "/app/:path*",
+// };
